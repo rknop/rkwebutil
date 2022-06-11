88937ba702e581b8822d8a167472292c67b77dfc
Rob Knop <rknop@pobox.com>
2022-06-11 11:26:46 -0400

    rkwebutil repository

---
+++ rkauth.js
@@ -0,0 +1,360 @@
+// Requires libraries:
+//   aes.js  ( From CryptoJS v3.1.2 rollups )
+//   jsencrypt.min.js  ( From http://travistidwell.com/jsencrypt/ )
+//
+// I don't have module versions of those, so you gotta include them in the page.
+
+import { rkWebUtil } from "./rkwebutil.js"
+
+// ASSUMPTIONS
+// * There is a div (passed to the constructor) that rkAuth can do whatever it wants to
+// * CSS classes .link and .center are defined
+
+var rkAuth = function( authdiv, webapurl, finishlogincallback, notauthcallback=null, errorhandler=null ) {
+    /** Handle authentication with auth.py server side
+     *
+     * Make one of these.  Pass it a div it can do whatever the hell it
+     * wants to, the URL for the webap (doesn't have to be absolute),
+     * and a default callback for when the user is authenticated.  It's
+     * expecting that the webap has /auth/... as defined by auth.py.
+     *
+     * Call the object's checkAuth method with a callback to call if authenticated,
+     * and one to call if not.  If isauthcallback is null, it'll call the
+     * finishlogicallback passed to the constructor.  If isnotauthcallback
+     * is null, it'll call showLoginUI.
+     *
+     * The object will have properties:
+     *    authenticated (true or false)
+     *
+     * and if authenticated is true:
+     *    username
+     *    useruuid
+     *    useremail
+     *    userdisplayname
+     *
+     * Also needed: resetpasswd_start.js
+     * **** Must edit that one to set webapurl **** (sad)
+     */
+
+    var self = this;
+    
+    this.authdiv = authdiv;
+    this.webapurl = webapurl;
+    this.finishlogincallback = finishlogincallback;
+    if ( notauthcallback != null )
+        this.notauthcallback = notauthcallback;
+    else
+        this.notauthcallback = function() { self.showLoginUI() };
+    if ( errorhandler != null )
+        this.errorhandler = errorhandler;
+    else
+        this.errorhandler = function( e ) { window.alert( e.error ) };
+    this.authenticated = false;
+    this.conn = new rkWebUtil.Connector( webapurl );
+}
+
+// **********************************************************************
+
+rkAuth.CryptoJSformatter = {
+    stringify: function( cipherParams ) {
+        var jsonobj = { ct: cipherParams.ciphertext.toString( CryptoJS.enc.Base64 ) };
+        if ( cipherParams.iv )
+            jsonobj.iv = cipherParams.iv.toString();
+        if ( cipherParams.salt )
+            jsonobj.salt = cipherParams.salt.toString();
+        return JSON.stringify( jsonobj );
+    },
+    parse: function( jsontext ) {
+        var jsonobj = JSON.parse( jsontext );
+        var cipherParams = CryptoJS.lib.CipherParams.create(
+            { ciphertext: CryptoJS.enc.Base64.parse( jsonobj.ct ) } );
+        if ( jsonobj.iv )
+            cipherParams.iv = CryptoJS.enc.Hex.parse( jsonobj.iv );
+        if ( jsonobj.salt )
+            cipherParams.salt = CryptoJS.enc.Hex.parse( jsonobj.salt );
+        return cipherParams;
+    }
+};
+
+// **********************************************************************
+
+rkAuth.prototype.checkAuth = function( isauthcallback=null, isnotauthcallback=null ) {
+    let self = this
+    this.conn.sendHttpRequest( "/auth/isauth", null,
+                               function( statedata ) {
+                                   self.processCheckAuth( statedata, isauthcallback, isnotauthcallback ) },
+                               self.errorhandler );
+}
+
+rkAuth.prototype.processCheckAuth = function( statedata, isauthcallback, isnotauthcallback ) {
+    // console.log( "In processCheckAuth, statedata:" );
+    // for ( const prop in statedata ) {
+    //     console.log( prop + " : " + statedata[prop] );
+    // }
+    if ( ! statedata.hasOwnProperty( "status" ) ) {
+        this.errorhandler( { 'error': 'Unexpected response from server authentication check; things are broken.' } );
+        return;
+    }
+    if ( statedata.status ) {
+        this.authenticated = true;
+        this.username = statedata.username;
+        this.useruuid = statedata.useruuid;
+        this.useremail = statedata.useremail;
+        this.userdisplayname = statedata.userdisplayname;
+        if ( isauthcallback != null )
+            isauthcallback();
+        else
+            this.finishlogincallback();
+    }
+    else {
+        this.authenticated = false;
+        if ( isnotauthcallback != null )
+            isnotauthcallback();
+        else
+            this.notauthcallback();
+    }
+}
+    
+
+// **********************************************************************
+
+rkAuth.prototype.logout = function( loggedoutcallback=null ) {
+    var self=this;
+    rkWebUtil.wipeDiv( this.authdiv );
+    rkWebUtil.elemaker( "p", this.authdiv, { "text": "...logging out..." } )
+    this.conn.sendHttpRequest( "auth/logout", null,
+                               function() { self.loggedout( loggedoutcallback ), self.errorhandler } );
+}
+
+rkAuth.prototype.loggedout = function( loggedoutcallback ) {
+    this.authenticated = false;
+    this.username = null;
+    this.useruuid = null;
+    this.useremail = null;
+    this.userdisplayname = null;
+    if ( loggedoutcallback != null ) {
+        loggedoutcallback()
+    } else {
+        rkWebUtil.wipeDiv( this.authdiv );
+        rkWebUtil.elemaker( "p", this.authdiv, { "text": "Logged out." } );
+    }
+}
+
+// **********************************************************************
+
+rkAuth.prototype.showLoginUI = function() {
+    let table, tr, td, button, p;
+    let self = this;
+    
+    rkWebUtil.wipeDiv( this.authdiv );
+
+    table = rkWebUtil.elemaker( "table", this.authdiv );
+    tr = rkWebUtil.elemaker( "tr", table );
+    td = rkWebUtil.elemaker( "td", tr, { "text": "Username:" } );
+    td = rkWebUtil.elemaker( "td", tr );
+    this.username_input = rkWebUtil.elemaker( "input", td,
+                                              { "attributes": { "type": "text",
+                                                                "size": 20,
+                                                                "id": "login_username" } } );
+    tr = rkWebUtil.elemaker( "tr", table );
+    td = rkWebUtil.elemaker( "td", tr, { "text": "Password:" } );
+    td = rkWebUtil.elemaker( "td", tr );
+    this.username_input = rkWebUtil.elemaker( "input", td,
+                                              { "attributes": { "type": "password",
+                                                                "size": 20,
+                                                                "id": "login_password" } } );
+    tr = rkWebUtil.elemaker( "tr", table );
+    td = rkWebUtil.elemaker( "td", tr, { "colspan": 2 } );
+    button = rkWebUtil.elemaker( "button", td, { "text": "Log In",
+                                                 "click": function() { self.startLogin() } } );
+
+    p = rkWebUtil.elemaker( "p", this.authdiv, { "text": "Request Password Reset",
+                                                 "click": function() { self.passwordLinkUI() } } );
+    p.classList.add( "link" );
+}
+
+// **********************************************************************
+
+rkAuth.prototype.startLogin = function() {
+    let self = this;
+    let username = document.getElementById( "login_username" ).value;
+    let password = document.getElementById( "login_password" ).value;
+    let requestdata = { 'username': username };
+    this.conn.sendHttpRequest( "/auth/getchallenge", requestdata,
+                               function( statedata ) {
+                                   self.processChallenge( statedata, password ) },
+                               self.errorhandler );
+}                                   
+
+// **********************************************************************
+
+rkAuth.prototype.processChallenge = function( retdata, password ) {
+    var self = this;
+    var rsadecrypt, privkey, response, requestdata;
+
+    if ( ( ! retdata.hasOwnProperty( "privkey" ) ) ||
+         ( ! retdata.hasOwnProperty( "challenge" ) ) ) {
+        this.errorhandler( { 'error': 'Unexpected reseponse from server password challenge; things are broken.' } );
+        return;
+    }
+    
+    try {
+        privkey = CryptoJS.AES.decrypt( retdata.privkey, password,
+                                        { format: rkAuth.CryptoJSformatter } ).toString( CryptoJS.enc.Utf8 );
+    }
+    catch( err )
+    {
+        this.errorhandler( { 'error': 'Incorrect username or password' } )
+        return;
+    }
+
+    rsadecrypt = new JSEncrypt();
+    rsadecrypt.setPrivateKey( privkey );
+    // Experimentally, JSEncrypt.decrypt wants base64 data
+    response = rsadecrypt.decrypt( retdata.challenge );
+
+    requestdata = { 'username': retdata.username,
+                    'response': response };
+    this.conn.sendHttpRequest( "/auth/respondchallenge", requestdata,
+                               function( statedata ) { self.processChallengeResponse( statedata ) },
+                               self.errorhandler );
+}
+
+rkAuth.prototype.processChallengeResponse = function( retdata ) {
+    if ( retdata.hasOwnProperty( 'error' ) ) {
+        if ( this.errorhandler != null ) {
+            this.errorhandler( retdata )
+        }
+        else {
+            window.alert( 'Challgene response returned error: ' + retdata.error );
+        }
+        return
+    }
+
+    if ( ( ! retdata.hasOwnProperty( 'status' ) ) ||
+         ( ! retdata.hasOwnProperty( 'message' ) ) ) {
+        this.errorhandler( { 'error': 'Unexpected response from server challenge response; things are broken.' } );
+        return;
+    }
+
+    this.authenticated = true;
+    this.username = retdata.username;
+    this.useruuid = retdata.useruuid;
+    this.useremail = retdata.useremail;
+    this.userdisplayname = retdata.userdisplayname;
+    this.finishlogincallback( retdata );
+}
+
+// **********************************************************************
+
+rkAuth.prototype.passwordLinkUI = function() {
+    let table, tr, td, button;
+    let self = this;
+    
+    rkWebUtil.wipeDiv( this.authdiv );
+
+    table = rkWebUtil.elemaker( "table", this.authdiv );
+    tr = rkWebUtil.elemaker( "tr", table );
+    td = rkWebUtil.elemaker( "td", tr, { "text": "Username:" } );
+    td = rkWebUtil.elemaker( "td", tr );
+    this.username_input = rkWebUtil.elemaker( "input", td,
+                                              { "attributes": { "type": "text",
+                                                                "size": 20,
+                                                                "id": "login_username" } } );
+    tr = rkWebUtil.elemaker( "tr", table );
+    td = rkWebUtil.elemaker( "td", tr, { "text": "—or—",
+                                         "attributes": { "colspan": 2 } } );
+    td.classList.add( "center" );
+    tr = rkWebUtil.elemaker( "tr", table );
+    td = rkWebUtil.elemaker( "td", tr, { "text": "e-mail:" } );
+    td = rkWebUtil.elemaker( "td", tr );
+    this.email_input = rkWebUtil.elemaker( "input", td,
+                                           { "attributes": { "type": "text",
+                                                             "size": 32,
+                                                             "id": "login_email" } } );
+    tr = rkWebUtil.elemaker( "tr", table );
+    td = rkWebUtil.elemaker( "td", tr, { "attributes": { "colspan": 2 } } );
+    button = rkWebUtil.elemaker( "button", td, { "text": "Email Password Reset Link",
+                                                 "click": function() { self.requestPasswordLink() } } );
+}
+
+// **********************************************************************
+
+rkAuth.prototype.requestPasswordLink = function() {
+    let self = this;
+    let data = { "username": this.username_input.value.trim(),
+                 "email": this.email_input.value.trim() };
+    let conn = new rkWebUtil.Connector( this.webapurl );
+    let p = rkWebUtil.elemaker( "p", this.authdiv, { "text": "Sending password reset link... please wait..." } );
+    conn.sendHttpRequest( "/auth/getpasswordresetlink", data,
+                          function( res ) { self.passwordLinkSent( res ) },
+                          function( res ) { window.alert( 'Error: ' + res.error ) } );
+}
+
+// **********************************************************************
+
+rkAuth.prototype.passwordLinkSent = function( res ) {
+    var self = this;
+    rkWebUtil.wipeDiv( this.authdiv );
+    rkWebUtil.elemaker( "p", this.authdiv, { "text": res.status } );
+    rkWebUtil.elemaker( "p", this.authdiv, { "text": "Back to login",
+                                             "classes": [ "link" ],
+                                             "click": function() { self.checkAuth() } } );
+}
+
+// **********************************************************************
+
+rkAuth.prototype.getPrivKey = function() {
+    let self = this;
+    let password = document.getElementById( "reset_password" ).value;
+    let confirmpassword = document.getElementById( "reset_confirm_password" ).value;
+    if ( confirmpassword != password ) {
+        window.alert( "Passwords do not match." );
+        return;
+    }
+    let resetuuid = document.getElementById( "resetpasswd_linkid" ).value;
+    this.conn.sendHttpRequest( "/auth/getkeys", { "passwordlinkid": resetuuid },
+                               function( statedata ) {
+                                   self.setNewPassword( statedata, password, resetuuid );
+                               },
+                               self.errorhandler );
+}          
+
+// **********************************************************************
+
+rkAuth.prototype.setNewPassword = function( statedata, password, resetuuid ) {
+    let self = this;
+    let encprivkey = CryptoJS.AES.encrypt( statedata.privatekey, password,
+                                           { format: rkAuth.CryptoJSformatter }).toString();
+    this.conn.sendHttpRequest( "/auth/changepassword",
+                               { "passwordlinkid": resetuuid,
+                                 "publickey": statedata.publickey,
+                                 "privatekey": encprivkey },
+                               function( statedata ) {
+                                   self.confirmChangePassword( statedata );
+                               },
+                               self.errorhandler );
+}
+
+// **********************************************************************
+
+rkAuth.prototype.confirmChangePassword = function( statedata ) {
+    if ( statedata.hasOwnProperty( "status" ) ) {
+        window.alert( statedata.status );
+    }
+    else {
+        window.alert( "Unexpected response." );
+        return;
+    }
+
+    rkWebUtil.wipeDiv( this.authdiv );
+    let p = document.createElement( "p" );
+    this.authdiv.appendChild( p );
+    p.appendChild( document.createTextNode( "Password change complete." ) );
+}    
+
+
+
+// **********************************************************************
+
+export { rkAuth }
---
+++ rkwebutil.js
@@ -0,0 +1,290 @@
+var rkWebUtil = {}
+
+// **********************************************************************
+// **********************************************************************
+// **********************************************************************
+// Utility functions
+
+rkWebUtil.wipeDiv = function( div )
+{
+    if ( div != null )
+        while ( div.firstChild )
+            div.removeChild( div.firstChild );
+}
+
+// **********************************************************************
+
+rkWebUtil.elemaker = function( elemtype, parent, inprops )
+{
+    var props = { text: null, click: null, classes: null, attributes: null };
+    Object.assign( props, inprops );
+    var text = props.text
+    var click = props.click;
+    var classes = props.classes;
+    var attributes = props.attributes;
+
+    var attr;
+    
+    var elem = document.createElement( elemtype );
+    if ( parent != null) parent.appendChild( elem );
+    if ( text != null )
+        elem.appendChild( document.createTextNode( text ) );
+    if ( click != null )
+        elem.addEventListener( "click", click );
+    if ( classes != null ) {
+        for ( let classname of classes ) {
+            elem.classList.add( classname )
+        }
+    }
+    if ( attributes != null ) {
+        for ( attr in attributes ) {
+            if ( attributes.hasOwnProperty( attr ) ) {
+                elem.setAttribute( attr, attributes[attr] );
+            }
+        }
+    }
+    return elem;
+}
+
+// **********************************************************************
+
+rkWebUtil.button = function( parent, title, callback )
+{
+    var button = document.createElement( "input" );
+    button.setAttribute( "type", "button" );
+    button.setAttribute( "value", title );
+    button.addEventListener( "click", callback );
+    if ( parent != null ) parent.appendChild( button );
+    return button;
+}
+
+// **********************************************************************
+// If I ever get a date that doesn't start "2020-07-15 07:42:00" (with
+// any old character in place of the space), I'm in trouble.  Alas,
+// I haven't found a reliable library routine to do this, because
+// Javascript insists on dates being local.
+
+rkWebUtil.parseStandardDateString = function( datestring )
+{
+    // console.log( "Trying to parse date string " + datestring );
+    var year = Number( datestring.substring(0, 4) );
+    var month = Number( datestring.substring(5, 7) );
+    var day = Number( datestring.substring( 8, 10) );
+    var hour = Number( datestring.substring( 11, 13) );
+    var minute = Number( datestring.substring( 14, 16) );
+    var second = Number( datestring.substring( 17, 19) );
+    var date;
+    date = new Date( Date.UTC( year, month-1, day, hour, minute, second ) );
+    // console.log( "y=" + year + " m=" + month + " d=" + day +
+    //              "h=" + hour + " m=" + minute + " s=" + second );
+    // console.log( "Date parsed: " + date.toString() );
+    return date;
+}
+
+// **********************************************************************
+
+rkWebUtil.dateFormat = function(date)
+{
+    // console.log(" Trying to format date " + date );
+    var result = "";
+    result += ("0000" + date.getFullYear().toString()).substr(-4, 4);
+    result += "-";
+    result += ("00" + (date.getMonth()+1).toString()).substr(-2, 2);
+    result += "-";
+    result += ("00" + date.getDate().toString()).substr(-2, 2);
+    result += " ";
+    result += ("00" + date.getHours().toString()).substr(-2, 2);
+    result += ":";
+    result += ("00" + date.getMinutes().toString()).substr(-2, 2);
+    result += ":";
+    result += ("00" + date.getSeconds().toString()).substr(-2, 2);
+    return result;
+}
+
+rkWebUtil.dateUTCFormat = function(date)
+{
+    var result = "";
+    result += ("0000" + date.getUTCFullYear().toString()).substr(-4, 4);
+    result += "-";
+    result += ("00" + (date.getUTCMonth()+1).toString()).substr(-2, 2);
+    result += "-";
+    result += ("00" + date.getUTCDate().toString()).substr(-2, 2);
+    result += " ";
+    result += ("00" + date.getUTCHours().toString()).substr(-2, 2);
+    result += ":";
+    result += ("00" + date.getUTCMinutes().toString()).substr(-2, 2);
+    result += ":";
+    result += ("00" + date.getUTCSeconds().toString()).substr(-2, 2);
+    return result;
+}
+
+rkWebUtil.validateWidgetDate = function( datestr ) {
+    if ( datestr.value.trim() == "" ) {
+        datestr.value = ""
+        return;
+    }
+    let num = Date.parse( datestr.value.trim() );
+    if ( isNaN(num) ) {
+        window.alert( "Error parsing date " + datestr.value
+                      + "\nNote that Javascript's Date.parse() doesn't seem to like AM or PM at the end; "
+                      + "use 24-hour time." );
+        return;
+    }
+    let date = new Date();
+    date.setTime( num );
+    // TODO : verify that both Firefox and Chrome (at least) parse this right
+    // (I don't just use toISOString because the T at the middle and all the precision is ugly for the user)
+    datestr.value = date.getUTCFullYear() + "-"
+        + String(date.getUTCMonth()+1).padStart( 2, '0') + "-"
+        + String(date.getUTCDate()).padStart( 2, '0' ) + " "
+        + String(date.getUTCHours()).padStart( 2, '0') + ":"
+        + String(date.getUTCMinutes()).padStart(2, '0') + ":"
+        + String(date.getUTCSeconds()).padStart( 2, '0') + "+00";
+}
+
+// **********************************************************************
+
+rkWebUtil.hideOrShow = function( widget, parameter, hideparams, showparams, displaytype="block" ) {
+    if ( showparams.includes( parameter ) ) {
+        widget.style.display = displaytype;
+    }
+    else if ( hideparams.includes( parameter ) ) {
+        widget.style.display = "none";
+    }
+    else {
+        window.alert( "Programmer error: rkWebUtil.hideOrShow unknown parameter " + parameter );
+    }
+}
+
+// **********************************************************************
+// **********************************************************************
+// **********************************************************************
+// Class for connecting to a given web server
+
+rkWebUtil.Connector = function( app )
+{
+    this.app = app;
+}
+
+// **********************************************************************
+// Utility function used in HTTP request callbacks to make
+//  sure the data is in.  Returns false if the data is not in,
+//  returns true if it is.  Returns null if there
+//  was an error (including non-JSON response).
+//
+// errorhandler is a function that takes a single argument
+// that argument will have property "error" which is a string message
+
+rkWebUtil.Connector.prototype.waitForJSONResponse = function( request, errorhandler = null )
+{
+    var type;
+
+    // console.log( "request.readyState = " + request.readyState +
+    //              ", request.stauts = " + request.status );
+    if (request.readyState === 4 && request.status === 200) {
+        type = request.getResponseHeader("Content-Type");
+        if (type === "application/json")
+        {
+            return true;
+        }
+        else
+        {
+            if ( errorhandler != null ) {
+                errorhandler( { "error": "Request didn't return JSON" }  );
+            }
+            else {
+                window.alert("Request didn't return JSON.  Everything is broken.  Panic.");
+            }
+            return null;
+        }
+    }
+    else if (request.readyState == 4) {
+        if ( errorhandler != null ) {
+            errorhandler( { "error": "Got back HTTP status " + request.status } );
+        }
+        else {
+            window.alert("Woah, got back status " + request.status + ".  Everything is broken.  Panic.");
+        }
+        return null;
+    }
+    else {
+        return false;
+    }
+}
+
+// **********************************************************************
+// Utility funciton to created and send an XMLHttpRequest, and wait
+// for it to be fully finished before calling the handler.
+//   appcommand -- the thing after the webapp, e.g. "/getquestionset"
+//   data -- an object to be converted with JSON.stringify and sent
+//   handler -- a function to be called when the request has fully returned
+//              it will have one argument, the data from the request
+//   errorhandler -- a function that takes a single argument that will
+//                   have property "error" which is a string message
+
+rkWebUtil.Connector.prototype.sendHttpRequest = function( appcommand, data, handler, errorhandler = null )
+{
+    let self = this;
+    let req = new XMLHttpRequest();
+    req.open( "POST", this.app + appcommand );
+    req.onreadystatechange = function() { self.catchHttpResponse( req, handler, errorhandler=errorhandler ) };
+    req.setRequestHeader( "Content-Type", "application/json" );
+    req.send( JSON.stringify( data ) );
+}
+
+rkWebUtil.Connector.prototype.sendHttpRequestMultipartForm = function( appcommand, formdata,
+                                                                       handler, errorhandler = null )
+{
+    let self = this;
+    let req = new XMLHttpRequest();
+    req.open( "POST", this.app + appcommand );
+    req.onreadystatechange = function() { self.catchHttpResponse( req, handler, errorhandler ) };
+    req.send( formdata );
+}
+
+rkWebUtil.Connector.prototype.catchHttpResponse = function( req, handler, errorhandler = null )
+{
+    if ( ! this.waitForJSONResponse( req, errorhandler ) ) return;
+    try {
+        var statedata = JSON.parse( req.responseText );
+    } catch (err) {
+        window.alert( "Error parsing JSON! (" + err + ")" );
+        console.trace();
+        console.log( req.responseText );
+    }
+    if ( statedata.hasOwnProperty( "error" ) ) {
+        if ( errorhandler != null ) {
+            errorhandler( statedata );
+        }
+        else {
+            window.alert( 'Error return: ' + statedata.error );
+        }
+        return;
+    }
+    handler( statedata );
+}
+
+// **********************************************************************
+// I'm honestly not sure how I want to format text, but I THINK I
+// want to have paragraphs separated by \n\n.  This strips all the <p>
+// and </p> tags from the text to move to that convention.
+
+rkWebUtil.stripparagraphtags = function(text)
+{
+    var verystartpar = new RegExp("^\s*<p>", "g");
+    var startpar = new RegExp("\n\s*<p>", "g");
+    var endpar = new RegExp("</p>\s*\n", "g");
+    var veryendpar = new RegExp("</p>\s*$", "g");
+
+    var newtext = text.replace(verystartpar, "");
+    newtext = newtext.replace(startpar, "\n");
+    newtext = newtext.replace(endpar, "\n");
+    newtext = newtext.replace(veryendpar, "");
+
+    return newtext;
+}
+
+// **********************************************************************
+
+export { rkWebUtil }
+
